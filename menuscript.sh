1d8edab8c6b7ac59b315ce0e1b296625c803a0e3
Connor <80653133+connorHashDash@users.noreply.github.com>
2024-10-02 20:12:57 +0100

    system controls get killed on certain applications booting

--- menuscript.sh
+++ menuscript.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-/usr/bin/antimicrox --hidden &
-
-# Infinite loop for the menu
+# Infinite loop for the menu so it comes back should you want to choose something else
 while true; do
-    CHOICE=$(whiptail --title "Barnwell Entertainment Centre" --menu "Please Select" 15 60 4 \
-        "1" "Films" \
-        "2" "Retrogames" \
-        "3" "Go to Desktop" 3>&2 2>&1 1>&3)
+
+sudo /usr/bin/antimicrox --hidden &
+
+ANTIMICROX_PID=$!
+
+    CHOICE=$(whiptail --title "Mini PC Menu" --menu "Choose an option" 15 60 4 \
+    "1" "Start Kodi" \
+    "2" "Start RetroArch" \
+    "3" "Start Wii U" \
+    "4" "Start Switch" \
+    "5" "Go to Desktop" 3>&2 2>&1 1>&3)
 
     case $CHOICE in
         1)
             echo "Starting Kodi..."
+	   kill $ANTIMICROX_PID
             kodi
             ;;
         2)
             echo "Starting RetroArch..."
+	    kill $ANTIMICROX_PID
             retroarch
             ;;
         3)
-            echo "Starting Desktop Environment..."
-            break
-            ;;
+            echo "Starting Wii U"
+            info.cemu.Cemu  
+	    ;;
+        4)
+            echo "Starting Switch"
+            org.ryujinx.Ryujinx
+	    ;;
         *)
             echo "Exiting..."
             break
@@ -29,3 +40,5 @@ while true; do
     esac
 done
 
+# Stop AntiMicroX when exiting 
+kill $ANTIMICROX_PID
